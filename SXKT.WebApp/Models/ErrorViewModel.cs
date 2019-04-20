464cd51be5969d1f6b95a45fe42e1eca16ceaae3
DucLM1 <DucLM1@daivietgroup.net.vn>
2019-04-20 09:30:19 +0700

    Add project files.

---
+++ SXKT.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SXKT.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
